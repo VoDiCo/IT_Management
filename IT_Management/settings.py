a23bfb2aa4fdf810fb72ae80840df396acf506f2
VoDiCo <nikovorinjski@googlemail.com>
2022-01-16 13:46:51 +0100

    added requirements.txt, .env and .gitignore. Changes settings.py to use env variables.

--- IT_Management/settings.py
+++ IT_Management/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR/'.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rqoetjiz*r#4e*=q^r-vpo)4&u9q#$m1rlt+%0=fuz7r6lk&-c'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
