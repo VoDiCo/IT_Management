157cc8d6c4c2661f8fc93e312a2ed1c143212316
VoDiCo <nikovorinjski@googlemail.com>
2022-01-16 13:07:34 +0100

    Just a commit for Tests

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import User, Department
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext, gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+
 
 # Register your models here.
 class UserAdminConfig(UserAdmin):
@@ -10,16 +11,18 @@ class UserAdminConfig(UserAdmin):
     list_filter = ('lname', 'fname', 'email', 'staff', 'admin',)
     search_fields = ('lname', 'fname', 'email',)
     fieldsets = (
-        (None,{'fields': ('email',)}),
-        (_('Personal Info'), {'fields': ('lname', 'fname', )}),
-        (_('Permissions'), {'fields': ( 'is_active', 'staff', 'admin',)}),
+        (None, {'fields': ('email',)}),
+        (_('Personal Info'), {'fields': ('lname', 'fname',)}),
+        (_('Permissions'), {'fields': ('is_active', 'staff', 'admin',)}),
         (_('Group'), {'fields': ('groups', 'user_permissions')})
-   )
+    )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'fname', 'lname', 'department', 'password1', 'password2'),
         }),
     )
+
+
 admin.site.register(User, UserAdminConfig)
 admin.site.register(Department)
