cc0f00a59709cb991930fffff26f64fef198ea0c
VoDiCo <nikovorinjski@googlemail.com>
2022-01-19 20:32:28 +0100

    repaired webpack by new install node-modules, added Component SideBar to frontend/src/component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
-
+import TemporaryDrawer from "./SideBar";
 
 export default class App extends Component {
   constructor(props) {
@@ -10,9 +10,14 @@ export default class App extends Component {
 
   render() {
     return (
+        <div>
       <div>
-        <HomePage />
+        <TemporaryDrawer/>
       </div>
+      <div>
+        <HomePage/>
+      </div>
+          </div>
     );
   }
 }
